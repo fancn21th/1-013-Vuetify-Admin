ca19c682778b63a11a7cc17f06fd210b3f76de52
fancn21th <fancn21th@gmail.com>
2023-02-23 14:46:31 +0800

    style customized with scss

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,22 +5,25 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+// import "vuetify/styles";
+import "@/config/main.scss";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+// import { md1, md2, md3 } from "vuetify/blueprints";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  // blueprint: md1,
   theme: {
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: "#1867C0",
+          secondary: "#5CBBF6",
         },
       },
     },
   },
-})
+});
